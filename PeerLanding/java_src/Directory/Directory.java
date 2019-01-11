23e8757b51f21ca5db27e6e1e979d14946daee0e
juan89 <a78074@alunos.uminho.pt>
2019-01-11 01:52:12 +0000

    Algumas alterações diretório.
    Ainda não está funcional.

--- PeerLanding/java_src/Directory/Directory.java
+++ PeerLanding/java_src/Directory/Directory.java
@@ -3,6 +3,7 @@ package Directory;
 import Directory.Representations.*;
 import Directory.Resources.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -13,6 +14,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -117,8 +119,33 @@ public class Directory {
     public LicitacaoRep getLicitacaoEmissao(@PathParam("nome") String nome, @PathParam("idE") int idE, @PathParam("id") int id){
         return new LicitacaoRep(empresas.get(nome).getEmissao(idE).getLicitacao(id));
     }
-
+    /*
     public static void main(String[] args) throws JsonProcessingException {
+        Licitacao lic1 = new Licitacao(1,"psa",20,500);
+        Licitacao lic2 = new Licitacao(2,"juan",30,100);
+        List<Licitacao> lics = new ArrayList<>();
+        Licitacao lic3 = new Licitacao(1,"m",30,200);
+        List<Licitacao> lics2 = new ArrayList<>();
+        lics.add(lic1);
+        lics.add(lic2);
+        lics2.add(lic3);
+        Emissao em1 = new Emissao(1,30,540,"naosei",true,lics);
+        Emissao em2 = new Emissao(2,43,520,"naosei",true,lics2);
+        List<Emissao> ems = new ArrayList<>();
+        ems.add(em1);
+        ems.add(em2);
+        List<Leilao> lls = new ArrayList<>();
 
-    }
+        Empresa emp = new Empresa("naosei",lls,ems);
+
+        HashMap<String, Empresa> empresas = new HashMap<>();
+
+        empresas.put("naosei", emp);
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String value = objectMapper.writeValueAsString(empresas);
+
+        System.out.print(value);
+    }*/
 }
